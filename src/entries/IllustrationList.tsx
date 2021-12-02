3418c8ae8323fca91de1f502f9e1c186b4421a6b
Anton Strogonoff <anton@strogonoff.name>
2021-12-02 19:48:35 +0100

    feat(IllustrationList): support adding, removing

--- src/entries/IllustrationList.tsx
+++ src/entries/IllustrationList.tsx
@@ -1,12 +1,15 @@
 /** @jsx jsx */
 /** @jsxFrag React.Fragment */
 
-import React from 'react';
+import React, { useContext } from 'react';
 import { jsx, css } from '@emotion/react';
+import { Button } from '@blueprintjs/core';
 
+import { DatasetContext } from '@riboseinc/paneron-extension-kit/context';
 import makeSearchResultList from '@riboseinc/paneron-extension-kit/widgets/SearchResultList';
 
 import { getIlloQueryExp } from '../query';
+import ActionButton from '../common/ActionButton';
 
 
 const IllustrationList: React.VoidFunctionComponent<{
@@ -16,18 +19,40 @@ const IllustrationList: React.VoidFunctionComponent<{
   onSelect: (filename: string | null) => void
   className?: string
 }> = function ({ categoryID, entrySlug, selected, onSelect, className }) {
+  const { addFromFilesystem, performOperation, operationKey } = useContext(DatasetContext);
+
   const queryExpression = getIlloQueryExp(
     categoryID,
     entrySlug,
   );
 
+  async function _handleAdd() {
+    if (addFromFilesystem) {
+      await addFromFilesystem(
+        { prompt: "Please select an image file", allowMultiple: true },
+        `added illustrations for ${entrySlug}`,
+        `${categoryID}/${entrySlug}`,
+        { offloadToLFS: true });
+    }
+  }
+  const canAdd = !operationKey && addFromFilesystem !== undefined;
+  const handleAdd = performOperation('adding illustrations', _handleAdd);
+
   return (
-    <SearchResultList
-      className={className}
-      queryExpression={queryExpression}
-      selectedItemPath={selected}
-      onSelectItem={onSelect}
-    />
+    <div className={className} css={css`display: flex; flex-flow: column nowrap;`}>
+      <div css={css`flex: 1;`}>
+        <SearchResultList
+          queryExpression={queryExpression}
+          selectedItemPath={selected}
+          onSelectItem={onSelect}
+        />
+      </div>
+      <ActionButton
+          disabled={!canAdd}
+          onClick={canAdd ? handleAdd : undefined}>
+        Add illustrations
+      </ActionButton>
+    </div>
   );
 }
 
@@ -38,13 +63,38 @@ export default IllustrationList;
 const Illustration: React.FC<{ objectData: any, objectPath: string }> =
 function ({ objectPath }) {
   const filename = objectPath.split('/')[3];
+  const { updateObjects, performOperation } = useContext(DatasetContext);
+
+  async function _handleDelete() {
+    if (updateObjects) {
+      await updateObjects({
+        commitMessage: `deleted illustration ${objectPath}`,
+        objectChangeset: {
+          [objectPath]: { newValue: null },
+        },
+        _dangerouslySkipValidation: true,
+      });
+    }
+  }
+  const handleDelete = performOperation('deleting illustration', _handleDelete);
+
   return (
     <span css={css`
         display: flex;
         flex-flow: row nowrap;
         align-items: center;
       `}>
-      {filename}
+      <span css={css`flex: 1;`}>
+        {filename}
+      </span>
+      <Button
+        small
+        minimal
+        title="Delete this image"
+        icon="cross"
+        onClick={handleDelete}
+        disabled={!updateObjects}
+      />
     </span>
   );
 }
